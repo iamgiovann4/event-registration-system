06e1a525950453b4151e363e56d2eb76565eaa6b
vitor <vitormariotto03@gmail.com>
2024-06-05 19:36:01 -0300

    teste deploy

--- backend/database/database.php
+++ backend/database/database.php
@@ -1,8 +1,9 @@
 <?php
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'cdevent';
+$servername = 'br-asc-web815.main-hosting.eu';
+$username = 'u455152201_cdevent';
+$password = 'Vg14072904';
+$database = 'u455152201_cdevent';
+
 // $servername = 'mysql.hostinger.com.br';
 // $username = 'u455152201_nossosite';
 // $password = 'fNS2VXU6CF:1';
